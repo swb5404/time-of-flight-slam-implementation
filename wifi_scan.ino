177a3c41d1083c072542df1994ac99b9397c6f83
Sarah Baker <66573140+swb5404@users.noreply.github.com>
2023-01-25 19:41:35 -0500

    Update wifi_scan.ino

--- wifi_scan.ino
+++ wifi_scan.ino
@@ -1,8 +1,4 @@
-/*
- *  This sketch demonstrates how to scan WiFi networks.
- *  The API is almost the same as with the WiFi Shield library,
- *  the most obvious difference being the different file you need to include:
- */
+
 #include "WiFi.h"
 
 void setup()
