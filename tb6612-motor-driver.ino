bcf8a16bf4ba694a6f0284abf1ffc15dc22e0285
Sarah Baker <66573140+swb5404@users.noreply.github.com>
2023-03-29 22:17:16 -0400

    Update tb6612-motor-driver.ino
    NEMA-17 12V stepper motor interfaced with TB6612 motor driver and ESP32 Devkit pins 19 18 5 17

--- tb6612-motor-driver.ino
+++ tb6612-motor-driver.ino
@@ -1,17 +1,20 @@
 #include <Stepper.h>
 
-// change this to the number of steps on your motor
 #define STEPS 200
-
+#define BIN2 19
+#define BIN1 18
+#define AIN1 5
+#define AIN2 17
+  
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
 // attached to
-Stepper stepper(STEPS, 4, 5, 6, 7);
+Stepper stepper(STEPS, BIN2, BIN1, AIN1, AIN2);
 
 
 void setup()
 {
-  // match baud rate
+  // match baud rate of serial monitor
   Serial.begin(115200);
   Serial.println("Stepper test!");
   // set to match with read rate of tof sensor
