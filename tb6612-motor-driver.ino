04c91e2b486a8366cdfbc9bc6d3ff2ce00ea89c7
Sarah Baker <66573140+swb5404@users.noreply.github.com>
2023-04-26 09:59:20 -0400

    Update tb6612-motor-driver.ino

--- tb6612-motor-driver.ino
+++ tb6612-motor-driver.ino
@@ -6,25 +6,15 @@
 #define AIN1 5
 #define AIN2 17
   
-// create an instance of the stepper class, specifying
-// the number of steps of the motor and the pins it's
-// attached to
 Stepper stepper(STEPS, BIN2, BIN1, AIN1, AIN2);
 
 
 void setup()
 {
-  // match baud rate of serial monitor
-  Serial.begin(115200);
-  Serial.println("Stepper test!");
-  // set to match with read rate of tof sensor
   stepper.setSpeed(15);
 }
 
 void loop()
 {
-  Serial.println("Forward");
-  stepper.step(STEPS);
-  Serial.println("Backward");
   stepper.step(STEPS);
 }
